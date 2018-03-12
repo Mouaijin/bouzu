88c3e64fe0b62879d4f675e9319ea54ad65a46ef
Christopher Davis <ui0177@InspiredThinking.com>
2018-03-12 18:38:18 -0400

    thinking about memory access

--- src/mmu.rs
+++ src/mmu.rs
@@ -35,17 +35,17 @@ impl Mmu {
             interrupts: 0,
         }
     }
-    pub fn read8(&self, addr:u16) -> u8{
-        match addr{
+    pub fn read8(&self, addr: u16) -> u8 {
+        match addr {
             //rom memory banks
             0x0000...0x7fff => self.rom.read8(addr),
             0x8000...0x9fff => self.vram[0x8000 - addr as usize],
             //external ram (handled by cartridge)
             0xa000...0xbfff => self.rom.read8(addr),
             //work ram 0
-            0xc000...0xcfff =>  self.work_ram_0[0xa000 - addr as usize],
+            0xc000...0xcfff => self.work_ram_0[0xa000 - addr as usize],
             //work ram 1..n
-            0xd000...0xdfff =>  self.work_ram_1[0xd000 - addr as usize],
+            0xd000...0xdfff => self.work_ram_1[0xd000 - addr as usize],
             //echo ram
             0xe000...0xfdff => self.read8(addr - 0x2000),
             //sprite table
@@ -58,29 +58,54 @@ impl Mmu {
             0xff80...0xfffe => self.hram[0xff80 - addr as usize],
             //interrupt register
             0xffff => self.interrupts,
-            _ => 0
+            _ => 0,
         }
     }
-    pub fn read16(&self, addr: u16) -> u16{
-        if addr < 0xffff{
-            join_u8(self.read8(addr), self.read8(addr+1))
-        }
-        else{
+    // pub fn ref8(&self, addr: u16) -> u8 {
+    //     match addr {
+    //         //rom memory banks
+    //         0x0000...0x7fff => self.rom.read8(addr),
+    //         0x8000...0x9fff => self.vram[0x8000 - addr as usize],
+    //         //external ram (handled by cartridge)
+    //         0xa000...0xbfff => self.rom.read8(addr),
+    //         //work ram 0
+    //         0xc000...0xcfff => self.work_ram_0[0xa000 - addr as usize],
+    //         //work ram 1..n
+    //         0xd000...0xdfff => self.work_ram_1[0xd000 - addr as usize],
+    //         //echo ram
+    //         0xe000...0xfdff => self.read8(addr - 0x2000),
+    //         //sprite table
+    //         0xfe00...0xfe9f => self.sprite_table[0xfe00 - addr as usize],
+    //         //unusable, I'll just return a 0
+    //         0xfea0...0xfeff => 0,
+    //         //io registers
+    //         0xff00...0xff7f => self.io_registers[0xff00 - addr as usize],
+    //         //hram
+    //         0xff80...0xfffe => self.hram[0xff80 - addr as usize],
+    //         //interrupt register
+    //         0xffff => self.interrupts,
+    //         _ => 0,
+    //     }
+    // }
+    pub fn read16(&self, addr: u16) -> u16 {
+        if addr < 0xffff {
+            join_u8(self.read8(addr), self.read8(addr + 1))
+        } else {
             //todo: probably wrong
             //this just left-shifts the last address's value 8 places when reading past the last byte
             join_u8(self.read8(addr), 0)
         }
     }
-    pub fn write8(&mut self, addr: Addr, dat : u8){
-        match addr{
+    pub fn write8(&mut self, addr: Addr, dat: u8) {
+        match addr {
             //rom memory banks
             0x8000...0x9fff => self.vram[0x8000 - addr as usize] = dat,
             //external ram (handled by cartridge)
             // 0xa000...0xbfff => self.rom.read8(addr),
             //work ram 0
-            0xc000...0xcfff =>  self.work_ram_0[0xa000 - addr as usize] = dat,
+            0xc000...0xcfff => self.work_ram_0[0xa000 - addr as usize] = dat,
             //work ram 1..n
-            0xd000...0xdfff =>  self.work_ram_1[0xd000 - addr as usize] = dat,
+            0xd000...0xdfff => self.work_ram_1[0xd000 - addr as usize] = dat,
             //echo ram
             0xe000...0xfdff => self.work_ram_0[addr as usize - 0x2000] = dat,
             //sprite table
@@ -93,11 +118,11 @@ impl Mmu {
             0xff80...0xfffe => self.hram[0xff80 - addr as usize] = dat,
             //interrupt register
             0xffff => self.interrupts = dat,
-            _ => ()
+            _ => (),
         }
     }
-    pub fn write16(&mut self, addr: Addr, dat : u16){
-        let (hi,lo) = split_u16(dat);
+    pub fn write16(&mut self, addr: Addr, dat: u16) {
+        let (hi, lo) = split_u16(dat);
         self.write8(addr, hi);
         self.write8(addr + 1, lo);
     }
